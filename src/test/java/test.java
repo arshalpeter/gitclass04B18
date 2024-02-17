34dbbebe1485b11e10e5cbae7251454e8bc599ec
arshalpeter <149265122+arshalpeter@users.noreply.github.com>
2024-02-17 12:29:50 -0500

    "Palent1"

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("I am batch 18 student");
+        System.out.println("I am batch 18 student");
+        System.out.println("I am batch 18 student");
     }
 }
